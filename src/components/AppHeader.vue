485952ab760b6186bfe8e751ca339fdf3d7e9b93
DavidePeloso-dev <davide.peloso.e@gmail.com>
2024-02-18 17:20:41 +0100

    Add app header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            searchTitle: ""
+        }
+    },
+    components: {
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="row d-flex jf-between">
+                <div class="logo">
+                    <img src="/boolflix-logo.svg" alt="logo">
+                </div>
+                <div class="search d-flex gap-3 al-center">
+                    <input type="text" name="search" id="search" v-model="searchTitle">
+                    <label for="search"><i class="fa-solid fa-magnifying-glass"></i></label>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header {
+    background-color: var(--boolflix-dark);
+}
+
+.search {
+    color: var(--boolflix-light);
+}
+</style>
