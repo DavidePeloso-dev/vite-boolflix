0d07418cdd42ac1ac030047a18b4bb6db68c9106
DavidePeloso-dev <davide.peloso.e@gmail.com>
2024-02-20 22:00:24 +0100

    Add strars vote

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -27,6 +27,10 @@ export default {
             if (movie.title == movie.original_title || movie.original_name) {
                 return false
             } else { return true }
+        },
+        voteStars(movie) {
+            console.log(Math.round(movie.vote_average * 5 / 10));
+            return Math.round(movie.vote_average * 5 / 10)
         }
     },
     props: {
@@ -38,9 +42,9 @@ export default {
     <main>
         <div class="container">
             <div class="row d-flex">
-                <div class="col-2" v-for="movie in state.movies">
+                <div class="col-2" v-show="movie.poster_path" v-for="movie in state.movies">
                     <div class="card">
-                        <img :src="state.img_prefix + movie.poster_path" alt="">
+                        <img v-if="movie.poster_path" :src="state.img_prefix + movie.poster_path" alt="">
                         <ul>
                             <li v-if="unike(movie)">{{ movie.title }}</li>
                             <li>{{ movie.original_title || movie.original_name }}</li>
@@ -48,7 +52,11 @@ export default {
                                 {{ movie.original_language }}
                                 <i class="ms-1 fp fp-rounded" :class="languageIcon(movie)"></i>
                             </li>
-                            <li>{{ movie.vote_average }}</li>
+                            <li v-if="movie.poster_path">
+                                <i v-for="n in voteStars(movie)" class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                <i v-for="n in (5 - voteStars(movie))" class="fa-regular fa-star"
+                                    style="color: #FFD43B;"></i>
+                            </li>
                         </ul>
                     </div>
                 </div>
