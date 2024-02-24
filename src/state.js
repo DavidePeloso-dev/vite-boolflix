8508718a68809e563978f3b5ec9ee8d781e99546
DavidePeloso-dev <davide.peloso.e@gmail.com>
2024-02-24 15:14:49 +0100

    Delete old parameter don't needed

--- src/state.js
+++ src/state.js
@@ -4,7 +4,6 @@ export const state = reactive({
     api_prefix: 'https://api.themoviedb.org/3',
     api_key: 'api_key=3f97b7c50ae937b02e10eacd3ff55f96',
     searchTitle: "",
-    searchType: "multi?",
     movies: [],
     img_prefix: "https://image.tmdb.org/t/p/",
     movieInfo: {},
